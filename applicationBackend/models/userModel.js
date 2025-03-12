33c527d7ce25b6010ec334abf6aafa7374c802e3
Ilysse200 <ilysseted500@gmail.com>
2025-03-12 16:17:17 +0200

    created userModel

---
+++ applicationBackend/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const {model, Schema} = mongoose;
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+const user = model('user',userSchema);
