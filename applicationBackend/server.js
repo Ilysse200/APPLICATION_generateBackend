ea0c4aaadf3231dedf436d97f077470d65866a36
Ilysse200 <ilysseted500@gmail.com>
2025-03-14 13:08:53 +0200

    modified backend for fetching on the frontend to take place

--- applicationBackend/server.js
+++ applicationBackend/server.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv"
+import cors from "cors";
 // const express = require('express');
 import bodyParser from 'body-parser';
 import formRouter from './routers/forrmRoutes.js';
 import applicationsRoute from './routers/applicationsRoute.js'
 import userRouter from './routers/userRoutes.js';
-
 dotenv.config();
 const app = express();
+const db_user = process.env.DB_USER;
+const db_name = process.env.DB_NAME;
+const port = process.env.PORT || 3000;
+const db_pass = process.env.DB_PASS;
 app.use(bodyParser.json());
+const corsOptions = {
+  origin: "*", // Accept requests from any origin
+  optionsSuccessStatus: 200,
+  credentials: true, // Allow cookies & authentication headers
+};
+
+app.use(cors(corsOptions));
+
 
 //Create a path to reach the form road
 
@@ -18,10 +30,7 @@ app.use('/applications',applicationsRoute)
 app.use('/users', userRouter)
 
 //variables that hold vvalues inside the .env file
-const db_user = process.env.DB_USER;
-const db_name = process.env.DB_NAME;
-const port = process.env.PORT || 3000;
-const db_pass = process.env.DB_PASS;
+
 
 
 //copied codes
