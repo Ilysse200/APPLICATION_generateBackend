c1f34dc2b4124f9c666f1ec95b4a3c5d39a97452
Ilysse200 <ilysseted500@gmail.com>
2025-03-24 16:21:35 +0200

    Added the routes for the recently added get method

--- applicationBackend/routers/forrmRoutes.js
+++ applicationBackend/routers/forrmRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createForm, displayFoms,getFormsById,deleteFormById,  updateFormById, getDepartments} from '../controller/formController.js'
+import {createForm, displayFoms,getFormsById,deleteFormById,  updateFormById, getDepartments, getJobPositions} from '../controller/formController.js'
 //A map to get all product-related routes
 const formRouter = express.Router();
 formRouter.post('/createForm', createForm); //road to create form
@@ -8,4 +8,5 @@ formRouter.get('/getformsById/:id', getFormsById);//road to get a specific form
 formRouter.delete('/deleteForm/:id',deleteFormById); //road to delete a specific form
 formRouter.put('/updateForm/:id', updateFormById);// road to update form
 formRouter.get('/getdepartment', getDepartments); //road to get predefined departments
+formRouter.get('/departmentJobs',getJobPositions)
 export default formRouter;
