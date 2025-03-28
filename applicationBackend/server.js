48e46b2bc8f66fff48c7ea90e955ea6e38c377c8
Ilysse200 <ilysseted500@gmail.com>
2025-03-28 07:53:48 +0200

    modified server.js

--- applicationBackend/server.js
+++ applicationBackend/server.js
@@ -9,6 +9,7 @@ import applicationsRoute from './routers/applicationsRoute.js'
 import userRouter from './routers/userRoutes.js';
 import deptRouter from './routers/departmentRoutes.js';
 import jobRouter from './routers/jobsRoutes.js';
+import formBuilderRouter from './routers/formBuilderRoutes.js';
 dotenv.config();
 const app = express();
 const db_user = process.env.DB_USER;
@@ -32,6 +33,7 @@ app.use('/applications',applicationsRoute)
 app.use('/users', userRouter)
 app.use('/departments',deptRouter)
 app.use('/jobs',jobRouter)
+app.use('/formBuild',formBuilderRouter)
 
 //variables that hold vvalues inside the .env file
 
