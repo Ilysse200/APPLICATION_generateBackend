c26cb239344d918f885969967f05da53d539ab57
Ilysse200 <ilysseted500@gmail.com>
2025-04-02 16:04:14 +0200

    Added routes of event

---
+++ applicationBackend/routers/eventRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+    createEvent,
+    displayEvents,
+    getEventsById,
+    updateEventById,
+    deleteEvent
+} 
+from '../controller/eventController.js';
+
+const eventRouter = express.Router();
+
+eventRouter.post('/eventCreate', createEvent);
+eventRouter.get('/fetchEvent', displayEvents);
+eventRouter.put('/updateEvent/:id',updateEventById);
+eventRouter.delete('/deleteEvent/:id',deleteEvent);
+eventRouter.get('/conditionalDisplay',getEventsById)
+
+
+export default eventRouter;
