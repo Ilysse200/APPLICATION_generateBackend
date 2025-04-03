51243b0d8420d78097612671d7dd2c658b435198
Ilysse200 <ilysseted500@gmail.com>
2025-04-04 00:15:42 +0200

    Created trainings model

---
+++ applicationBackend/models/trainingsModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const {Schema, model} = mongoose;
+
+const trainingSchema = new Schema({
+
+    trainingName:{
+        type: String,
+        required: true
+    },
+    trainingObjective:{
+        type: String,
+        required: true
+    },
+    trainingDuration:{
+        type: String,
+        required: true
+    },
+    trainingMode:{
+        type:String,
+        enum:["Online", "Onsite", "Hybrid"],
+        required: true
+    },
+    trainingRequirements:{
+        type: String,
+        required: true
+    },
+    departmentId: {
+        type: mongoose.Schema.Types.ObjectId,
+            ref: 'Department',
+            required: true,
+    },
+
+})
+const trainings = model('trainings', trainingSchema);
+export default trainings;
