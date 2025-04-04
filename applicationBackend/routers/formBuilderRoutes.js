f923237ca1d5ca2695dfecf373da1eb76362e7d5
Ilysse200 <ilysseted500@gmail.com>
2025-04-04 05:26:21 +0200

    Customized builder routes

--- applicationBackend/routers/formBuilderRoutes.js
+++ applicationBackend/routers/formBuilderRoutes.js
@@ -1,18 +1,21 @@
+// routes/formBuilderRoutes.js
 import express from 'express';
 import {
-  createFormBlueprint,
-  getFormBlueprintByJob,
-  updateFormBlueprint,
-  deleteFormBlueprint,
-  displayFormsBuilder
+  createOrUpdateBlueprint,
+  getBlueprintByVacancy,
+  deleteBlueprint,
+  getBlueprintByVacancyAndId,
+  getAllBlueprints
 } from '../controller/formBuilderController.js';
+import upload from '../middleware/multer.js';
 
 const formBuilderRouter = express.Router();
 
-formBuilderRouter.post('/createBluePrint', createFormBlueprint);
-formBuilderRouter.get('/formBlueprints/:jobId', getFormBlueprintByJob);
-formBuilderRouter.put('/modifyBlueprint/:jobId', updateFormBlueprint);
-formBuilderRouter.delete('/deleteBlueprint/:jobId', deleteFormBlueprint);
-formBuilderRouter.get('/displayForms', displayFormsBuilder)
+formBuilderRouter.post('/blueprint',upload.any(), createOrUpdateBlueprint);
+formBuilderRouter.get('/blueprint/:vacancyType', getBlueprintByVacancy);
+formBuilderRouter.delete('/blueprint/:vacancyType', deleteBlueprint);
+formBuilderRouter.get('/blueprint/:vacancyType/:vacancyId', getBlueprintByVacancyAndId);
+formBuilderRouter.get('/allblueprints', getAllBlueprints);
+
 
 export default formBuilderRouter;
